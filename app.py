ed5f2492923eeaf0b0be5a269c918a0a7ea92e53
Krishna Nagar <notkrises@gmail.com>
2025-07-21 23:18:54 -0400

    learning strings

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-students_count = 1000
-rating = 4.99
-is_published = True
-is_published = False
-course_name = "Python Programming"
-print(students_count)
+course = "Python Programming"
+message = """
+Hi John,
+
+blah blah blah text between multiple lines
+"""
+print(len(course))  # len of the string
+print(course[0])  # prints the characters in place of index 0
+print(course[-1])  # prints the last character
+print(course[0:3])  # prints from the 1st to the 4th character
+print(course[0:])  # prints everything from the first letter and after
+print(course[:3])  # prints everything before the 4th character inclusive
