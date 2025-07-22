c9d1ffe45bfc38ea1552b55d4b35f36f6bff1541
Krishna Nagar <notkrises@gmail.com>
2025-07-22 00:04:38 -0400

    learning escape sequences

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
-course = "Python Programming"
-message = """
-Hi John,
 
-blah blah blah text between multiple lines
-"""
-print(len(course))  # len of the string
-print(course[0])  # prints the characters in place of index 0
-print(course[-1])  # prints the last character
-print(course[0:3])  # prints from the 1st to the 4th character
-print(course[0:])  # prints everything from the first letter and after
-print(course[:3])  # prints everything before the 4th character inclusive
+
+course = "Python \"Programming\""  # for quotes inside of quotes
+course = "Pyton \nProgramming"  # for new line
+course = "Python \\Programming"  # so the first \ allows for the second \
