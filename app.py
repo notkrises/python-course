3339a7a85b18888d3b35120bf57b8fff156fa4e3
Krishna Nagar <notkrises@gmail.com>
2025-07-22 20:10:55 -0400

    learning conditional statements

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-x = input("x: ")
-y = int(x) + 1
-"""
-y = x + 1 # gives error bc adding an integer to a str
-
-int(x) # converts into an int
-str(x) # converts into a str
-float(x) # converts into a float
-bool(x) # converts into a bool (0, "", and None are False, everything else is True)"""
-
-print(f"x: {x}, y: {y}")
+temp = 15
+if temp > 30:
+    print("It's hot, so drink water")
+elif temp > 20:
+    print("it's nice")
+else:
+    print("it's chilly")
+print("Done")
