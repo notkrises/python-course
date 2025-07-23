a9eccb29ac590f21380107cf722dd68937f7cfa7
Krishna Nagar <notkrises@gmail.com>
2025-07-22 20:55:11 -0400

    default arguments

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-def increment(number, by):
+def increment(number, by=1):  # makes the argument 1 by default and optional
     return number + by
+# all optional parameters should come AFTER required parameters
 
 
 # keyword argument, not necessary but allows reader to know what it stands for
-print(increment(2, by=1))
+print(increment(2, by=5))  # second argument not needed
